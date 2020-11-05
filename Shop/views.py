c544568b7c4010f12ce3f7179f657f8dd129630f
ShaxzodbekBozorov <shaxzodbekbozorov@gmail.com>
2020-11-05 23:27:55 +0500

    html files for product detail and product list

--- Shop/views.py
+++ Shop/views.py
@@ -14,7 +14,7 @@ def product_list(request, category_slug=None):  # if recieve category slug filte
                                                       'products': products})
 
 
-def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+def product_detail(request, product_id, slug):
+    product = get_object_or_404(Product, id=product_id, slug=slug, available=True)
     return render(request, 'shop/product/detail.hmtl', {'product': product})
 
